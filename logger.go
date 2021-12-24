2ccdc7de09005882ae20ed235e7aeaaf5da5a3d0
shouzhengli <shouzhengli@tencent.com>
2021-12-24 23:58:43 +0800

    <>:修改Logger定义

--- logger.go
+++ logger.go
@@ -6,9 +6,7 @@ import (
 )
 
 type Logger interface {
-	Fatalf(format string, v ...interface{})
 	Errorf(format string, v ...interface{})
-	Warnf(format string, v ...interface{})
 	Infof(format string, v ...interface{})
 	Debugf(format string, v ...interface{})
 }
